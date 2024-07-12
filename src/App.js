0d703524291170ed07706736c36420cb58ebcd49
Henok <htarekegn40@gmail.com>
2024-07-12 11:22:44 +0300

    updated wishlist

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
 import Billing from './Components/Billing/Billing';
+import Wishlist from './Components/Wishlist/Wishlist';
 
 function App() {
   return (
--- src/Components/Wishlist/Wishlist.jsx
+++ src/Components/Wishlist/Wishlist.jsx
@@ -1,12 +1,81 @@
 import React from 'react'
-
+import "./Wishlist.css"
 function Wishlist() {
   return (
-    <div className='wishlist-conatiner'>
-        <div className="header">
-            <h4>Wishlist (4)</h4>
-            <button>Move all To bag</button>
+    <div className='wishlist-container'>
+      <div className="header">
+        <h4>Wishlist (4)</h4>
+        <button>Move All To Bag</button>
+      </div>
+
+      <div class="wishlist-items-container">
+        <div className="wishlist">
+          <div className="wishlist-items">
+            <div className="discount">
+              <p>35%</p>
+            </div>
+            <div className="wishimg">
+              <img src="" alt="huge bag" />
+            </div>
+            <div className="delete">
+              <img src="" alt="delete" />
+            </div>
+
+            <button>Add to Cart</button>
+          </div>
+
+          {/* <div>
+            <p>Gucci Bag</p>
+            <p>$960</p>
+          </div> */}
         </div>
+        <div className="wishlist">
+          <div className="wishlist-items">
+            <div className="discount">
+              <p>35%</p>
+            </div>
+            <div className="wishimg">
+              <img src="" alt="huge bag" />
+            </div>
+            <div className="delete">
+              <img src="" alt="delete" />
+            </div>
+
+            <button>Add to Cart</button>
+          </div>
+
+        </div>
+        <div className="wishlist">
+          <div className="wishlist-items">
+            <div className="discount">
+              <p>35%</p>
+            </div>
+            <div className="wishimg">
+              <img src="" alt="huge bag" />
+            </div>
+            <div className="delete">
+              <img src="" alt="delete" />
+            </div>
+
+            <button>Add to Cart</button>
+          </div>
+        </div>
+        <div className="wishlist">
+          <div className="wishlist-items">
+            <div className="discount">
+              <p>35%</p>
+            </div>
+            <div className="wishimg">
+              <img src="" alt="huge bag" />
+            </div>
+            <div className="delete">
+              <img src="" alt="delete" />
+            </div>
+
+            <button>Add to Cart</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
