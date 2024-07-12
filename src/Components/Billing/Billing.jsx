27c1130e8bd26b240563bb06c0c0950cb07dd1d0
yabets143 <139507335+yabets143@users.noreply.github.com>
2024-07-12 10:36:13 +0300

    add socials icon on footer and edit billing component

--- src/Components/Billing/Billing.jsx
+++ src/Components/Billing/Billing.jsx
@@ -2,7 +2,36 @@ import React from 'react'
 import "./Billing.css"
 function Billing() {
     return (
-        <div className='bill'>Billing</div>
+        <div className='bill'>
+            <div className="bill_header">
+            <p>
+                <a>Account/</a>
+                <a>My Account/</a>
+                <a>Product/</a>
+                <a>View cart/</a>
+                <a>CheckOut/</a>
+            </p>
+            <h1>Billing Details</h1>
+
+            </div >
+            <div className="bill_body">
+                <div className="bill_form">
+                
+                </div>
+                <div className="bill_summary">
+                <div className="bill_items">
+                <img/><span>LCD Monitor</span>
+                <h3>$650</h3>
+                <img/><span>LCD Monitor</span>
+                <h3>$650</h3>
+                </div>
+                <p>Subtotal</p><span>$1750</span>
+
+                </div>
+
+            </div>
+
+        </div>
     )
 }
 
