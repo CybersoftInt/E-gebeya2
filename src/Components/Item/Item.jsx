8502c832982f20a1f95a6fee6a90fbae96de0139
yabets143 <139507335+yabets143@users.noreply.github.com>
2024-07-15 12:07:33 +0300

    edit on item component

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 function Item() {
     return (
-        <div>Item</div>
+        <div className='item'>
+            <div className="item_dir">
+                <p><span></span></p>
+            </div>
+           <div className="item_details">
+           <div className="item_images">
+                
+                </div>
+            <div className="item_disc">
+                
+            </div>
+           </div>
+            
+        </div>
     )
 }
 
