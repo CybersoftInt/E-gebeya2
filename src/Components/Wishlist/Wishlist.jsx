68ae7fd429d1b5c2de8713192ca2fd5912da0280
Henok <htarekegn40@gmail.com>
2024-07-15 10:31:42 +0300

    Updated About

--- src/Components/Wishlist/Wishlist.jsx
+++ src/Components/Wishlist/Wishlist.jsx
@@ -91,7 +91,95 @@ function Wishlist() {
           </div>
         </div>
       </div>
+      <div className="wishlist-for-you">
+        <h4>Just For You</h4>
+        <button>See All</button>
+      </div>
+      <div class="wishlist-items-container">
+        <div className="wishlist">
+          <div className="wishlist-items">
+            <div className="discount">
+              <p>35%</p>
+            </div>
+            <div className="delete">
+              {/* <img src="" alt="delete" /> */}
+
+            </div>
+            <div className="wishimg">
+              {/* <img src="" alt="huge bag" /> */}
+            </div>
+            <button>Add to Cart</button>
+            <div className="par">
+              <p className="p1">Playstation Joystick</p>
+              <p className="p2">590 <span>1000</span></p>
+            </div>
+
+
+          </div>
+
+
+        </div>
+        <div className="wishlist">
+          <div className="wishlist-items">
+            <div className="discount">
+              <p>35%</p>
+            </div>
+            <div className="wishimg">
+              {/* <img src="" alt="huge bag" /> */}
+            </div>
+            <div className="delete">
+              {/* <img src="" alt="delete" /> */}
+            </div>
+
+            <button>Add to Cart</button>
+            <div className="par">
+              <p className="p1">Playstation Joystick</p>
+              <p className="p2">590 <span>1000</span></p>
+            </div>
+          </div>
+
+        </div>
+        <div className="wishlist">
+          <div className="wishlist-items">
+            <div className="discount">
+              <p>35%</p>
+            </div>
+            <div className="wishimg">
+              {/* <img src="" alt="huge bag" /> */}
+            </div>
+            <div className="delete">
+              {/* <img src="" alt="delete" /> */}
+            </div>
+
+            <button>Add to Cart</button>
+            <div className="par">
+              <p className="p1">Playstation Joystick</p>
+              <p className="p2">590 <span>1000</span></p>
+            </div>
+          </div>
+        </div>
+        <div className="wishlist">
+          <div className="wishlist-items">
+            <div className="discount">
+              <p>35%</p>
+            </div>
+            <div className="wishimg">
+              {/* <img src="" alt="huge bag" /> */}
+            </div>
+            <div className="delete">
+              {/* <img src="" alt="delete" /> */}
+            </div>
+
+            <button>Add to Cart</button>
+            <div className="par">
+              <p className="p1">Playstation Joystick</p>
+              <p className="p2">590 <span>1000</span></p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+
   )
 }
 
--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,8 +1,82 @@
 import React from 'react'
-
+import "./About.css"
+import SideImage from "../../Components/Assets/About_Side Image.png"
+import IconShop from "../../Components/Assets/icon_shop.png"
 function About() {
     return (
-        <div>About</div>
+        <div>
+            <div className="story">
+                <div className="text">
+                    <h1>Our Story</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias molestiae earum, in magnam, et magni saepe vero quaerat accusantium optio ipsum maiores repellendus dolorem amet illo consectetur ipsam voluptatum corrupti.</p>
+                </div>
+                <img src={SideImage} alt="" />
+            </div>
+            <div className="counter">
+                <div className="stat">
+                    <div className="icon-stat">
+                        <div className="icon">
+                            <img src={IconShop} alt="" />
+                        </div>
+                        <div className="desc">
+                            <h3>10.5k</h3>
+                            <p>Sellers Active at Our Site</p>
+                        </div>
+                    </div>
+
+
+
+
+                </div>
+                <div className="stat">
+                    <div className="icon-stat">
+                        <div className="icon">
+                            <img src={IconShop} alt="" />
+                        </div>
+                        <div className="desc">
+                            <h3>10.5k</h3>
+                            <p>Sellers Active at Our Site</p>
+                        </div>
+                    </div>
+
+
+
+
+                </div>
+                <div className="stat">
+                    <div className="icon-stat">
+                        <div className="icon">
+                            <img src={IconShop} alt="" />
+                        </div>
+                        <div className="desc">
+                            <h3>10.5k</h3>
+                            <p>Sellers Active at Our Site</p>
+                        </div>
+                    </div>
+
+
+
+
+                </div>
+                <div className="stat">
+                    <div className="icon-stat">
+                        <div className="icon">
+                            <img src={IconShop} alt="" />
+                        </div>
+                        <div className="desc">
+                            <h3>10.5k</h3>
+                            <p>Sellers Active at Our Site</p>
+                        </div>
+                    </div>
+
+
+
+
+                </div>
+
+
+            </div>
+        </div>
     )
 }
 
