734002a34b0de50b0b9be5361b4d54716f6bf70e
yabets143 <139507335+yabets143@users.noreply.github.com>
2024-07-22 08:57:50 +0300

    update card and item components

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 function Card() {
   return (
-    <div>Card</div>
+    <div className='card'>
+      <div className="wishlist-items">
+            <div className="discount">
+              <p>{discount}</p>
+            </div>
+            <div className="wishimg">
+              <img src={wishimg} alt="huge bag" />
+            </div>
+            <div className="delete">
+              {/* <img src="" alt="delete" /> */}
+            </div>
+
+            <button>Add to Cart</button>
+            <div className="par">
+              <p className="p1">{itemName}</p>
+              <p className="p2">590 <span>1000</span></p>
+            </div>
+          </div>
+    </div>
   )
 }
 
