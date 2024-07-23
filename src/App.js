e3d07f30382880ac7a7facc3d988265fb2623679
Henok <htarekegn40@gmail.com>
2024-07-23 11:12:53 +0300

    Updated Footer.jsx and Footer.css

--- src/App.js
+++ src/App.js
@@ -63,7 +63,7 @@ function App() {
         <Route path='/cart' element={<Cart/>}/>
         <Route path='/login' element={<LoginSignup/>}/> */}
         </Routes>
-        {/* <Footer /> */}
+        <Footer />
       </BrowserRouter>
 
     </div>
