f3d289f168e2eed6244d12affdaa40e58fdee061
Henok <htarekegn40@gmail.com>
2024-07-23 11:31:24 +0300

    Updated Login.css and Login.jsx

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,33 +1,29 @@
-import React from 'react'
-import "../../Components/Login/Login.css"
-import SideImage from "../Assets/Login_Side Image.png"
-import { upload } from '@testing-library/user-event/dist/upload'
+import React from 'react';
+import "../../Components/Login/Login.css";
+import SideImage from "../Assets/Login_Side Image.png";
+
 function Login() {
     return (
         <div className='container'>
             <div className="img">
-                <img src={SideImage} alt="" />
+                <img src={SideImage} alt="Shopping illustration" />
             </div>
             <div className="login">
-
-
                 <div className="sub-login">
                     <h3>Log in</h3>
                     <p>Enter your details below</p>
-                    <label htmlFor="pass" >
-                        <input type="text" name="email" id="" placeholder='Email or Phone Number' />
-                        <input type="password" name="pass" id="" placeholder='Password' />
-                    </label>
+                    <div className="input-container">
+                        <input type="text" name="email" placeholder='Email or Phone Number' />
+                        <input type="password" name="pass" placeholder='Password' />
+                    </div>
                 </div>
                 <div className="btn">
                     <button>Log In</button>
-                    <span>Forgot Password?</span>
+                    <a href="#" className="forgot-password">Forgot Password?</a>
                 </div>
-
             </div>
-
         </div>
-    )
+    );
 }
 
-export default Login
+export default Login;
