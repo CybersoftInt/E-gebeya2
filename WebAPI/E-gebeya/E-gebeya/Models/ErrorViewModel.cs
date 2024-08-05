e6e102269cfce2371356bc71ec08f2211da74506
yabets143 <139507335+yabets143@users.noreply.github.com>
2024-08-05 12:01:07 +0300

    new api folder added

--- WebAPI/E-gebeya/E-gebeya/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace E_gebeya.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
