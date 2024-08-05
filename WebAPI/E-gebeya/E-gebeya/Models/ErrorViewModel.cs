1165c92cd4a5aae5165df3b03884b85503eed37d
Henok <htarekegn40@gmail.com>
2024-08-05 11:09:02 +0300

    Create WebAPI

---
+++ WebAPI/E-gebeya/E-gebeya/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace E_gebeya.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
